6ceb7778c1f70f2e7b0519db8d67f3cce1f5ad69
Rodrigo <SESA585994@se.com>
2020-12-30 18:43:18 -0300

    temp

--- src/app/public/components/user-redirection/user-redirection.component.ts
+++ src/app/public/components/user-redirection/user-redirection.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/core/services/auth.service';
+import { CardsService } from 'src/app/core/services/cards.service';
 import { LocalStorageKeys } from '../../../shared/constants'; 
 
 @Component({
@@ -9,10 +10,17 @@ import { LocalStorageKeys } from '../../../shared/constants';
 })
 export class UserRedirectionComponent implements OnInit {
 
-  constructor(public authService: AuthService) { }
+  constructor(
+    public authService: AuthService,
+    private cardsService: CardsService
+    ) { }
 
   ngOnInit(): void {
   }
 
   get userName() { return this.authService.getFromLocalStorage(LocalStorageKeys.USER_FIRST_NAME) };
+
+  onClickHome(){
+    this.cardsService.setCardVisible(true);
+  }
 }
